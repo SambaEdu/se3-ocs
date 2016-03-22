8f23846388fe36bdb6626c769561f99be50bb618
mollef <franck.molle@ac-rouen.fr>
2016-03-22 01:06:52 +0100

    to delete old version

--- sources/install-sql/clean-oldocs.sh
+++ sources/install-sql/clean-oldocs.sh
@@ -7,8 +7,10 @@ mysqladmin -f drop ocsweb
 rm -f /var/www/se3/includes/dbconfig.inc.php
 mysql -e "drop USER ocs@localhost" -b mysql
 mysql -e "drop USER ocs" -b mysql
-
-
+rm -f /etc/apache2se/conf.d/ocsinventory.conf
+rm -f /etc/apache2/conf.d/ocsreports.conf
+rm -fr /etc/ocsinventory
+exit 0
 
 
 
